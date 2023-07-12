bf21bc7417a9eff986b09aefde8b8aff70e3d1af
Matt Bellis <mbellis@siena.edu>
2023-07-12 14:32:43 -0400

    Update and rename cms_open_data_dictionary to cms_open_data_tools.py
    Moved things around.

--- cms_open_data_tools.py
+++ cms_open_data_tools.py
@@ -1,3 +1,4 @@
+###############################################################################################
 datasets = {}
 
 datasets['collision'] = {}
@@ -37,3 +38,36 @@ darkmatter_file = {"tag":'darkmatter_2015_0000', \
         "miniAOD_file": 'root://eospublic.cern.ch//eos/opendata/cms/mc/RunIIFall15MiniAODv2/DarkMatter_MonoPhoton_AV_Mx-500_Mv-10000_gDMgQ1_LO_13TeV-madgraph/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/80000/AA064591-DBD8-E511-A193-0025904C66EE.root', \
         "OpenDataPortal_link": 'https://opendata.cern.ch/record/16515'}
 datasets['MC']['Darkmatter'].append(darkmatter_file)
+
+###############################################################################################
+
+###############################################################################################
+def generate_nicks_recipe(x, number_of_events=-1):
+
+  tag_nevents = 'default'
+  if number_of_events == -1:
+    tag_nevents = 'ALL_EVENTS_IN_MINIAOD'
+  else:
+    tag_nevents = f'nevents_{number_of_events}'
+
+  miniAOD = f'DATA_miniAOD_{x[0]["tag"]}.root'
+  configuration_file = f'{x[0]["tag"]}_cfg.py'
+  print(f'xrdcp {x[0]["miniAOD_file"]} {miniAOD}\n')
+
+  for i in datasets['collision']:
+    if datasets['collision'][i] == x:
+      print(f'cmsDriver.py --python_filename {configuration_file} --eventcontent NANOAOD --datatier NANOAOD \
+  --fileout file:{x[0]["tag"]}_{tag_nevents}_nanoaod.root --conditions 106X_dataRun2_v36 --step NANO \
+  --filein file:{miniAOD}_miniaod.root --era Run2_25ns,run2_nanoAOD_106X2015 --no_exec --data -n {number_of_events} \
+  --customise PhysicsTools/PFNano/pfnano_cff.PFnano_customizeData_onlyPF\n')
+      print('collision')
+
+  for i in datasets['MC']:
+    if datasets['MC'][i] == x:
+      print(f'cmsDriver.py --python_filename {configuration_file} --eventcontent NANOAODSIM --datatier NANOAODSIM \
+        --fileout file:{x[0]["tag"]}_{tag_nevents}_nanoaod.root --conditions 102X_mcRun2_asymptotic_v8 --step NANO \
+        --filein file:{miniAOD} --era Run2_25ns,run2_nanoAOD_106X2015 --no_exec --mc -n {number_of_events} \
+        --customise PhysicsTools/PFNano/pfnano_cff.PFnano_customizeMC_onlyPF\n')
+      print('MC')
+
+  print(f'cmsRun {configuration_file}')
